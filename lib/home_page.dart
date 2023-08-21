5fd2a567ef87eb92518c38d50e26db7901c6914e
IT_big <yossapon@efinancethai.com>
2023-08-21 18:28:55 +0700

    full

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:isolate';
 import '';
 import 'package:flutter/foundation.dart';
@@ -27,6 +28,54 @@ Future<int> runHeavyTaskIWithoutIsolaterun(int args) async {
   }
 
   return value;
+}
+
+Future<int> runHeavyTaskIWithoutIsolatespawncommunication(
+    List<dynamic> args) async {
+  var mainToIsolateStream = ReceivePort();
+  var isolateToMainStream = args[0];
+
+  int value = 0;
+  for (var i = 0; i < args[1]; i++) {
+    value += i;
+  }
+  isolateToMainStream.send(mainToIsolateStream.sendPort);
+  mainToIsolateStream.listen((data) {
+    print('[mainToIsolateStream] $data');
+    //exit(0);
+  });
+  isolateToMainStream.send('This is from myIsolate()');
+  return value;
+}
+
+Future<dynamic> myMaininitIsolate() async {
+  Completer completer = Completer<SendPort>();
+  var isolateToMainStream = ReceivePort();
+
+  isolateToMainStream.listen((data) {
+    if (data is SendPort) {
+      var mainToIsolateStream = data;
+      completer.complete(mainToIsolateStream);
+    } else {
+      print('[isolateToMainStream] $data');
+    }
+  });
+  await Isolate.spawn(myIsolate, isolateToMainStream.sendPort);
+  //await Isolate.spawn(myIsolate, isolateToMainStream.sendPort);
+  return completer.future;
+}
+
+void myIsolate(SendPort isolateToMainStream) {
+  var mainToIsolateStream = ReceivePort();
+  runHeavyTaskIWithoutIsolaterun(4000000000);
+  isolateToMainStream.send(mainToIsolateStream.sendPort);
+
+  mainToIsolateStream.listen((data) {
+    print('[mainToIsolateStream] $data');
+    //exit(0);
+  });
+
+  isolateToMainStream.send('This is from myIsolate()');
 }
 
 Future<int> computeFactorial(int n) async {
@@ -67,121 +116,133 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const CircularProgressIndicator(),
-            Text(values.toString()),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - without Isolate'),
-              onPressed: () {
-                runHeavyTaskIWithoutIsolate([4000000000]);
-                setState(() {});
-              },
-            ),
-            Text(values1.toString()),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - with compute'),
-              onPressed: () async {
-                //computeFactorial(4);
-                //compute จำเป็นต้อง อยู่นอก state เลย
-                final data =
-                    await compute(runHeavyTaskIWithoutIsolates, 4000000000);
-                values1 = data;
-                print(data);
-                setState(() {});
-              },
-            ),
-            Text(values2.toString()),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - with isolate.run'),
-              onPressed: () async {
-                //computeFactorial(4);
-                //compute จำเป็นต้อง อยู่นอก state เลย
-                try {
-                  final data = await Isolate.run(() async {
-                    return runHeavyTaskIWithoutIsolaterun(4000000000);
-                    // int value = 0;
-                    // for (var i = 0; i < 40000000; i++) {
-                    //   value += i;
-                    // }
-
-                    // values = value;
-                    // return value;
-                  });
-                  values2 = data;
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const CircularProgressIndicator(),
+              Text(values.toString()),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - without Isolate'),
+                onPressed: () {
+                  runHeavyTaskIWithoutIsolate([4000000000]);
                   setState(() {});
-                  // runHeavyTaskIWithoutIsolates(40000000));
+                },
+              ),
+              Text(values1.toString()),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - with compute'),
+                onPressed: () async {
+                  //computeFactorial(4);
+                  //compute จำเป็นต้อง อยู่นอก state เลย
+                  final data =
+                      await compute(runHeavyTaskIWithoutIsolates, 4000000000);
+                  values1 = data;
                   print(data);
-                } on StateError catch (e, s) {
-                  print(e.message);
-                }
+                  setState(() {});
+                },
+              ),
+              Text(values2.toString()),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - with isolate.run'),
+                onPressed: () async {
+                  //computeFactorial(4);
+                  //compute จำเป็นต้อง อยู่นอก state เลย
+                  try {
+                    final data = await Isolate.run(() async {
+                      return runHeavyTaskIWithoutIsolaterun(4000000000);
+                      // int value = 0;
+                      // for (var i = 0; i < 40000000; i++) {
+                      //   value += i;
+                      // }
 
-                // final data = await Isolate.run(runHeavyTaskIWithoutIsolaterun);
-                // print(data);
+                      // values = value;
+                      // return value;
+                    });
+                    values2 = data;
+                    setState(() {});
+                    // runHeavyTaskIWithoutIsolates(40000000));
+                    print(data);
+                  } on StateError catch (e, s) {
+                    print(e.message);
+                  }
 
-                //print(data);
-              },
-            ),
-            BlocBuilder<IsoBloc, IsoState>(
-              builder: (context, state) {
-                if (state is IsoLoading) {
-                  return const CircularProgressIndicator();
-                } else if (state is IsoLoaded) {
-                  return Text('bloc example: ${state.value}');
-                } else {
-                  return const Text('');
-                }
-              },
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - Bloc'),
-              onPressed: () => context.read<IsoBloc>()..add(IsocountEvent()),
-            ),
-            const SizedBox(height: 20),
-            BlocBuilder<IsoCubit, IsoStates>(
-              builder: (context, state) {
-                if (state is IsoLoadings) {
-                  return const CircularProgressIndicator();
-                } else if (state is IsoLoadeds) {
-                  return Text('cubit example: ${state.value}');
-                } else {
-                  return const Text('');
-                }
-              },
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - Cubit'),
-              onPressed: () => context.read<IsoCubit>().IsocountEvent(),
-            ),
-            const SizedBox(height: 20),
-            Consumer(
-              builder: (context, ref, child) {
-                final state = ref.watch(countProvider);
-                if (state is IsoLoadingss) {
-                  return const CircularProgressIndicator();
-                } else if (state is IsoLoadedss) {
-                  return Text('cubit example: ${state.value}');
-                } else {
-                  return const Text('');
-                }
-              },
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              child: const Text('Run Heavy Task - Riverpods'),
-              onPressed: () =>
-                  ref.read(countProvider.notifier).IsocountEvents(),
-            )
-          ],
+                  // final data = await Isolate.run(runHeavyTaskIWithoutIsolaterun);
+                  // print(data);
+
+                  //print(data);
+                },
+              ),
+              Text(values2.toString()),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text(
+                    'Run Heavy Task - with isolate.spawn communication'),
+                onPressed: () async {
+                  var mainToIsolateStream = await myMaininitIsolate();
+                  mainToIsolateStream.send('This is from main() start ');
+                },
+              ),
+              BlocBuilder<IsoBloc, IsoState>(
+                builder: (context, state) {
+                  if (state is IsoLoading) {
+                    return const CircularProgressIndicator();
+                  } else if (state is IsoLoaded) {
+                    return Text('bloc example: ${state.value}');
+                  } else {
+                    return const Text('');
+                  }
+                },
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - Bloc'),
+                onPressed: () => context.read<IsoBloc>()..add(IsocountEvent()),
+              ),
+              const SizedBox(height: 20),
+              BlocBuilder<IsoCubit, IsoStates>(
+                builder: (context, state) {
+                  if (state is IsoLoadings) {
+                    return const CircularProgressIndicator();
+                  } else if (state is IsoLoadeds) {
+                    return Text('cubit example: ${state.value}');
+                  } else {
+                    return const Text('');
+                  }
+                },
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - Cubit'),
+                onPressed: () => context.read<IsoCubit>().IsocountEvent(),
+              ),
+              const SizedBox(height: 20),
+              Consumer(
+                builder: (context, ref, child) {
+                  final state = ref.watch(countProvider);
+                  if (state is IsoLoadingss) {
+                    return const CircularProgressIndicator();
+                  } else if (state is IsoLoadedss) {
+                    return Text('cubit example: ${state.value}');
+                  } else {
+                    return const Text('');
+                  }
+                },
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Text('Run Heavy Task - Riverpods'),
+                onPressed: () =>
+                    ref.read(countProvider.notifier).IsocountEvents(),
+              )
+            ],
+          ),
         ),
       ),
     );
